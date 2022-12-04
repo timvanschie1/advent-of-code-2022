2550388d74c67fbcbd6fa448762672bde23cd2d3
timvanschie1 <timvanschie1@gmail.com>
2022-12-04 22:54:27 +0100

    day 3 part 1

---
+++ day3/index.test.js
@@ -0,0 +1,15 @@
+const {test, expect} = require("@jest/globals");
+const part1 = require('./part1');
+const part2 = require('./part2');
+
+const testData = [
+    'vJrwpWtwJgWrhcsFMMfFFhFp',
+    'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL',
+    'PmmdzqPrVvPwwTWBwg',
+    'wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn',
+    'ttgJtRGJQctTZtZT',
+    'CrZsJsPPZsGzwwsLwLmpwMDw'
+]
+
+test('day 3, part 1', () => expect(part1(testData)).toBe(157));
+test('day 3, part 2', () => expect(part2(testData)).toBe(null));
