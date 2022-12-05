0ef22df84e58e53c32a9f7c3d799d854c750e22d
timvanschie1 <timvanschie1@gmail.com>
2022-12-05 21:11:49 +0100

    day 4 part 2

--- day4/part2.js
+++ day4/part2.js
@@ -1,6 +1,19 @@
 function part2(data) {
-    let answer;
-    return answer;
+    let nrOfOverlappingPairs = 0;
+
+    data.forEach(([one, two]) => {
+        const [oneMin, oneMax] = one.split('-').map(string => Number(string));
+        const [twoMin, twoMax] = two.split('-').map(string => Number(string));
+        const twoMinOverlapsOne = twoMin >= oneMin && twoMin <= oneMax;
+        const twoMaxOverlapsOne = twoMax >= oneMin && twoMax <= oneMax;
+        const twoContainsOne = oneMin >= twoMin && oneMax <= twoMax;
+
+        if (twoMinOverlapsOne || twoMaxOverlapsOne || twoContainsOne) {
+            nrOfOverlappingPairs++;
+        }
+    })
+
+    return nrOfOverlappingPairs;
 }
 
 module.exports = part2;
