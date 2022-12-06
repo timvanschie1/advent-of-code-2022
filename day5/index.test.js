dd70e2e0ce896e0b630b7365b49036d7fb370a7b
timvanschie1 <timvanschie1@gmail.com>
2022-12-06 08:02:03 +0100

    day 5 part 2

--- day5/index.test.js
+++ day5/index.test.js
@@ -13,4 +13,4 @@ move 2 from 2 to 1
 move 1 from 1 to 2`;
 
 test('day 5, part 1', () => expect(part1(testData)).toBe('CMZ'));
-// test('day 5, part 2', () => expect(part2(testData)).toBe(null));
+test('day 5, part 2', () => expect(part2(testData)).toBe('MCD'));
--- day5/part2.js
+++ day5/part2.js
@@ -1,5 +1,38 @@
 function part2(data) {
-    let answer;
+    const [stacksString, instructionsString] = data.split('\n\n');
+    const stacksStringLines = stacksString.split('\n');
+    const stackNumbers = stacksStringLines.pop().trim().split(/\s+/);
+    stacksStringLines.reverse();
+
+    const stacksObj = {}
+    stackNumbers.forEach((stackNumber, i) => {
+        stacksObj[stackNumber] = [];
+        const correspondingStringIndex = 1 + (i * 4);
+
+        stacksStringLines.forEach(stringLine => {
+            const crate = stringLine.substring(correspondingStringIndex, correspondingStringIndex + 1);
+            if (crate === ' ') return;
+            stacksObj[stackNumber].push(crate);
+        })
+    })
+
+    const instructionsStringLines = instructionsString.split('\n');
+    instructionsStringLines.forEach(stringLine => {
+        const [, amountString, , from, , to] = stringLine.split(' ');
+        const amount = Number(amountString);
+
+        const cratesToMove = [];
+        for (let i = 0; i < amount; i++) {
+            const crate = stacksObj[from].pop();
+            cratesToMove.unshift(crate);
+        }
+        stacksObj[to] = [...stacksObj[to], ...cratesToMove];
+    })
+
+    let answer = '';
+    stackNumbers.forEach((stackNumber) => {
+        answer = answer + stacksObj[stackNumber].pop();
+    })
     return answer;
 }
 
